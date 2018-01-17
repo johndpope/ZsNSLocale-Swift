1af0244b5a9629e83b7f93366b52a7dfedad49bf
mops_common <chenyunyun@haolianluo.com>
2018-01-17 18:54:04 +0800

    First Commit

--- ZsNSLocale-Swift/ViewController.swift
+++ ZsNSLocale-Swift/ViewController.swift
@@ -13,7 +13,45 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        let Locale:NSLocale = NSLocale.init(localeIdentifier: "zh_Hans_CN")
+//        NSLog("%@", Locale.localeIdentifier)
+//        NSLog("%@", Locale.displayName(forKey: NSLocale.Key.identifier, value: "zh_Hans_CN")!)
+//        NSLog("%@", Locale.displayName(forKey: NSLocale.Key.identifier, value: "en_US")!)
+        
+        
+//        let ENLocale:NSLocale = NSLocale.init(localeIdentifier: "en_US")
+//        NSLog("%@", ENLocale.localeIdentifier)
+//        NSLog("%@", ENLocale.displayName(forKey: NSLocale.Key.identifier, value: "zh_Hans_CN")!)
+//        NSLog("%@", ENLocale.displayName(forKey: NSLocale.Key.identifier, value: "en_US")!)
+        
+        
+//        let Locale:NSLocale = NSLocale.system as NSLocale
+//        NSLog("%@", Locale.localeIdentifier)
+//        NSLog("%@", Locale.languageCode)
+//        NSLog("%@", Locale.displayName(forKey: NSLocale.Key.identifier, value: "zh_Hans_CN")!)
+//        NSLog("%@", Locale.displayName(forKey: NSLocale.Key.identifier, value: "en_US")!)
+        
+//        let CurrenLocale:NSLocale = NSLocale.current as NSLocale
+//        NSLog("%@", CurrenLocale.localeIdentifier)
+//        NSLog("%@", CurrenLocale.displayName(forKey: NSLocale.Key.identifier, value: "zh_Hans_CN")!)
+//
+//        let NotifiCenter:NotificationCenter = NotificationCenter.default
+//        NotifiCenter.addObserver(self, selector: #selector(LocaleDidChangeNotification), name: NSLocale.currentLocaleDidChangeNotification, object: nil)
+        
+        let AutoUpdatingCurrenLocale:NSLocale = NSLocale.autoupdatingCurrent as NSLocale
+        NSLog("%@", AutoUpdatingCurrenLocale.localeIdentifier)
+        
+        
+        
     }
+    
+    @objc func LocaleDidChangeNotification(){
+    
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
